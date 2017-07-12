4130922246d0f9586b1f63d58df440d9ecccc15f
Deborah Oni <deborah.oni@andela.com>
2017-07-12 14:00:14 +0100

    ch(test) eslint corrections

--- server/migrations/20170527014009-create-doc-model.js
+++ server/migrations/20170527014009-create-doc-model.js
@@ -2,34 +2,34 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) =>
-     queryInterface.createTable('Docs', {
-       id: {
-         allowNull: false,
-         autoIncrement: true,
-         primaryKey: true,
-         type: Sequelize.INTEGER
-       },
-       userId: {
-         type: Sequelize.STRING
-       },
-       title: {
-         type: Sequelize.STRING
-       },
-       content: {
-         type: Sequelize.STRING
-       },
-       access: {
-         type: Sequelize.STRING
-       },
-       createdAt: {
-         allowNull: false,
-         type: Sequelize.DATE
-       },
-       updatedAt: {
-         allowNull: false,
-         type: Sequelize.DATE
-       }
-     }),
-  down: (queryInterface, Sequelize) =>
-   queryInterface.dropTable('Docs')
+    queryInterface.createTable('Docs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.STRING
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      access: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }),
+  down: queryInterface =>
+    queryInterface.dropTable('Docs')
 };
